a7cc225a77e4a8149a2a8a922f0022f196c35ca3
Qcactus <1123403518@qq.com>
2020-05-20 18:55:12 +0800

    add forms

--- backend/schoolmanagement-master/school/forms.py
+++ backend/schoolmanagement-master/school/forms.py
@@ -61,3 +61,75 @@ class ContactusForm(forms.Form):
     Name = forms.CharField(max_length=30)
     Email = forms.EmailField()
     Message = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+
+
+class CampusForm(forms.ModelForm):
+    class Meta:
+        model = models.Campus
+        fields = '__all__'
+
+
+class MajorForm(forms.ModelForm):
+    class Meta:
+        model = models.Major
+        fields = '__all__'
+
+
+class ClassesForm(forms.ModelForm):
+    class Meta:
+        model = models.Classes
+        fields = '__all__'
+
+
+class PeopleForm(forms.ModelForm):
+    class Meta:
+        model = models.People
+        fields = '__all__'
+
+
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = models.Notice
+        fields = '__all__'
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = models.Student
+        fields = '__all__'
+
+
+class StudyEventForm(forms.ModelForm):
+    class Meta:
+        model = models.StudyEvent
+        fields = '__all__'
+
+
+class ChangeMajorForm(forms.ModelForm):
+    class Meta:
+        model = models.ChangeMajor
+        fields = '__all__'
+
+
+class StudyDelayForm(forms.ModelForm):
+    class Meta:
+        model = models.StudyDelay
+        fields = '__all__'
+
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = models.Course
+        fields = '__all__'
+
+
+class StartedCourseInfoForm(forms.ModelForm):
+    class Meta:
+        model = models.StartedCourseInfo
+        fields = '__all__'
+
+
+class ChooseCourseForm(forms.ModelForm):
+    class Meta:
+        model = models.ChooseCourse
+        fields = '__all__'
